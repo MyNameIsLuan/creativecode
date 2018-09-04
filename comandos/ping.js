33f026efc4fba10f18d6d64b458b5715c59c193f
LockDzn <ryanssouza00@gmail.com>
2018-09-04 12:58:01 -0300

    :inbox_tray: | Novo comando !ping

--- comandos/ping.js
+++ comandos/ping.js
@@ -4,8 +4,10 @@ module.exports.run = async (client, message, args) => {
     
     const m = await message.channel.send(`:ping_pong: Pong!`);
     if(args[0] == "api") {
-        return message.channel.send(`:satellite: **|** ${message.author}, latência da API: ${Math.round(client.ping)}ms.`)
+        message.channel.send(`:satellite: **|** ${message.author}, latência da API: ${Math.round(client.ping)}ms.`)
+        return m.delete().catch(O_o=>{});
     }
     message.channel.send(`:satellite: **|** ${message.author}, latência de: ${m.createdTimestamp - message.createdTimestamp}ms.`)
+    m.delete().catch(O_o=>{});
 
 }
